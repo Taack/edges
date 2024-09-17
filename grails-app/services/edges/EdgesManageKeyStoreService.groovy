b7ecf23d10f3598db314341e2000f3909dda5782
Adrien Guichard <adrien.guichard@taack.org>
2024-09-17 04:48:37 +0200

    WiP manage KeyStore

---
+++ grails-app/services/edges/EdgesManageKeyStoreService.groovy
@@ -0,0 +1,19 @@
+package edges
+
+class EdgesManageKeyStoreService {
+
+/*
+    1.
+    ~/.jdks/jbrsdk-17.0.9-1/bin/keytool -genkey -keypass password -storepass password -keyalg RSA -keystore serverkeystore.jks
+    Quels sont vos nom et prénom ?
+    [Unknown]:  localhost
+    Quel est le nom de votre unité organisationnelle ?
+    [Unknown]:  CiO
+
+    2.
+    ~/.jdks/jbrsdk-17.0.9-1/bin/keytool -export -storepass password -file server.cer -keystore serverkeystore.jks
+
+    3.
+    ~/.jdks/jbrsdk-17.0.9-1/bin/keytool -import -v -trustcacerts -file server.cer -keypass password -storepass password -keystore clienttruststore.jks
+    */
+}
